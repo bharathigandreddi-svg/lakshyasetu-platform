01a478126237b91d0a2be917c63b4618989334d9
bharathigandreddi-svg <bharathigandreddi@gmail.com>
2026-08-20 10:52:57 +0530

    Fix Supabase project URL for student authentication

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.LAKSHYASETU_CONFIG = {
-  supabaseUrl: "https://byounbmdyuytoqqyhgos.supabase.co",
+  supabaseUrl: "https://byounbmdyuytoqkyhgos.supabase.co",
   supabasePublishableKey: "sb_publishable_tCvBH8eh95-nXOChd2_sLQ__iDZIfNa"
 };
 
